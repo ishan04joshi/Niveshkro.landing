99aee46eea2052a7428bc8706eb5e4c695c4906b
ishan04joshi <90335897+ishan04joshi@users.noreply.github.com>
2023-03-25 18:58:09 +0530

    LInks updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,6 +10,6 @@
 
 // !! DEVELOPMENT
 
-export const websiteURL = "https://staging.niveshkro.com";
-export const dashboardURL = "https://staging.app.niveshkro.com";
+export const websiteURL = "https://nivesh-landing-new.netlify.app/";
+export const dashboardURL = "https://app-niveshkro-new.netlify.app/";
 export const backendURL = "https://api.staging.niveshkro.com";
